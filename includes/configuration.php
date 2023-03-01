63505c4ba9c3cdf1da6d56e723ecb757ff44ee32
Dan White <dan.white1@wsu.edu>
2023-03-01 12:53:51 -0800

    Add scope for group data and fix some text

--- includes/configuration.php
+++ includes/configuration.php
@@ -8,7 +8,7 @@ define( 'OIDC_MIDDLEWARE_LOGOUT_CALLBACK_URL', 'http://' . DOMAIN_CURRENT_SITE .
 define( 'OIDC_LOGIN_TYPE', 'button' );
 define( 'OIDC_CLIENT_ID', getenv('OIDC_CLIENT_ID') );
 define( 'OIDC_CLIENT_SECRET', getenv('OIDC_CLIENT_SECRET') );
-define( 'OIDC_CLIENT_SCOPE', 'openid email profile' );
+define( 'OIDC_CLIENT_SCOPE', 'openid email profile umc_wp' );
 define( 'OIDC_ENDPOINT_LOGIN_URL', 'https://logintst.wsu.edu/oauth2/default/v1/authorize' );
 define( 'OIDC_ENDPOINT_USERINFO_URL', 'https://logintst.wsu.edu/oauth2/default/v1/userinfo' );
 define( 'OIDC_ENDPOINT_TOKEN_URL', 'https://logintst.wsu.edu/oauth2/default/v1/token' );
@@ -25,12 +25,10 @@ define( 'OIDC_DISPLAY_NAME_FORMAT', '{given_name} {family_name}' );
 define( 'OIDC_HIDE_SETTINGS_PAGE', true );
 
 
-// Customize plugin functionality via filters
+// Customize plugin functionality via filters & actions
 add_filter('openid-connect-generic-login-button-text', function( $text ) {
 
-    $text = 'Login with WSU SSO';
-
-    return $text;
+    return 'Login with WSU SSO';
 
 });
 
