02b7f423b0880b142776ebcea0ab6a486c46c9ce
Danial Bleile <danbleile@gmail.com>
2023-05-03 12:09:28 -0700

    Version | 4.0.2

--- openid-connect-generic.php
+++ openid-connect-generic.php
@@ -16,7 +16,7 @@
  * Plugin Name:       OpenID Connect Generic
  * Plugin URI:        https://github.com/daggerhart/openid-connect-generic
  * Description:       Connect to an OpenID Connect generic client using Authorization Code Flow.
- * Version:           4.0.1
+ * Version:           4.0.2
  * Requires at least: 4.9
  * Requires PHP:      7.2
  * Author:            daggerhart
